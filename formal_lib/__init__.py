0cb171f0e7858a03a3b80668f2887529a8bf0f3f
Yiannis Charalambous <yiannis128@hotmail.com>
2026-04-03 00:14:20 +0100

    add: re-export specs, SPECS registry, and detect_spec from top-level package

--- formal_lib/__init__.py
+++ formal_lib/__init__.py
@@ -3,9 +3,23 @@
 from formal_lib.__about__ import __version__
 from formal_lib.logging import logger
 from formal_lib.verifier_runner import VerifierRunner
+from formal_lib.specs import (
+    SPECS,
+    detect_spec,
+    cbmc_spec,
+    clang_spec,
+    esbmc_spec,
+    pytest_spec,
+)
 
 __all__ = [
     "__version__",
     "logger",
     "VerifierRunner",
+    "SPECS",
+    "detect_spec",
+    "cbmc_spec",
+    "clang_spec",
+    "esbmc_spec",
+    "pytest_spec",
 ]
